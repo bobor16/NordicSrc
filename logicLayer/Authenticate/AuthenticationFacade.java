2afee2f05f197142a08ed43a25be1be01152ec0e
Martin Sørensen <martin.se.sorensen@gmail.com>
2018-10-23 13:54:27 +0200

    classes in logic

--- logicLayer/Authenticate/AuthenticationFacade.java
+++ logicLayer/Authenticate/AuthenticationFacade.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package logicLayer.Authenticate;
 
 /**
  *
- * @author Borgar Bordoy
+ * @author Martin Sorensen
  */
-public class Test {
+public class AuthenticationFacade {
     
 }

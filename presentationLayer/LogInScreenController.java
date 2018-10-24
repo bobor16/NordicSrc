c827e365b1fac391ec6e2e626503f6ec0421d78c
Rasmus Hansen <rasmush92@live.com>
2018-10-24 12:51:40 +0200

    Ting

--- presentationLayer/LogInScreenController.java
+++ presentationLayer/LogInScreenController.java
@@ -5,16 +5,23 @@
  */
 package presentationLayer;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -24,17 +31,19 @@ import javafx.scene.input.KeyEvent;
 public class LogInScreenController implements Initializable {
 
     @FXML
-    private TextField UsernameBox;
+    private TextField EmailField;
     @FXML
-    private PasswordField PasswordBox;
+    private PasswordField PasswordField;
     @FXML
-    private Button LogOnButton;
+    private Button LoginButton;
     @FXML
-    private Button ResetPasswordButton;
+    private Button RegisterButton;
     @FXML
     private Label passwordMsgLabel;
+    @FXML
+    private Label ResetPasword;
 
-    /**
+    /*
      * Initializes the controller class.
      */
     @Override
@@ -43,15 +52,15 @@ public class LogInScreenController implements Initializable {
     }    
 
     @FXML
-    private void pressEnter(KeyEvent event) {
+    private void pressEnter(KeyEvent event) throws IOException {
     }
 
     @FXML
-    private void LogOnButtonAction(ActionEvent event) {
+    private void LoginOnAction(ActionEvent event) {
     }
 
     @FXML
-    private void ResetPasswordAction(ActionEvent event) {
+    private void RegisterOnAction(ActionEvent event) {
     }
     
 }

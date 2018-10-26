f9a23f493ee1b1d8a01896899d9c59c6c9a3c265
Rasmus Hansen <rasmush92@live.com>
2018-10-26 12:07:39 +0200

    test

--- presentationLayer/CasePortalViewController.java
+++ presentationLayer/CasePortalViewController.java
@@ -3,12 +3,33 @@ package presentationLayer;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
 
 
 public class CasePortalViewController implements Initializable {
 
-    /**
+    @FXML
+    private ListView<?> CaseListView;
+    @FXML
+    private Label CaseIDLabel;
+    @FXML
+    private Label OrderTitleLabel;
+    @FXML
+    private Label QuantityLabel;
+    @FXML
+    private Label DeliveryDateLabel;
+    @FXML
+    private AnchorPane rootPane;
+    
+    
+
+    /*
      * Initializes the controller class.
      */
     @Override
@@ -16,4 +37,8 @@ public class CasePortalViewController implements Initializable {
         // TODO
     }    
     
+    private void loadScene2(ActionEvent event) throws IOExeotion {
+        AnchorPane pane = FXMLLoader.load(getClass().getResources("Test.fxml"));
+        rootPane.getChildren().set
+    }
 }
